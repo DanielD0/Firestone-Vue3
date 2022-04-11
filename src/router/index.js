d2038a4674aeeebfc584103cbc4b48ef80a0ceff
DanielD0 <37585619+DanielD0@users.noreply.github.com>
2022-04-11 17:05:21 -0500

    capitulo 176

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: () => import(/* webpackChunkName: "about" */ '../views/Home.vue')
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/Perfil',
+    name: 'Perfil',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    component: () => import(/* webpackChunkName: "about" */ '../views/Perfil.vue')
+  },
+  {
+    path: '/crud',
+    name: 'Crud',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Crud.vue')
+  },
 ]
 
 const router = createRouter({
--- src/views/Crud.vue
+++ src/views/Crud.vue
@@ -1,13 +1,13 @@
 <template>
-  <h1>HOME</h1>
+  <h1>CRUD</h1>
 </template>
 
 <script>
-
 export default {
-  name: 'Home',
-  components: {
-  
-  }
+
 }
 </script>
+
+<style>
+
+</style>
