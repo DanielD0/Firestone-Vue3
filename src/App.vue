cf964e95522c782234f85bd1473ff2a4304fefa9
DanielD0 <37585619+DanielD0@users.noreply.github.com>
2022-04-15 20:10:56 -0500

    Capitulo 183

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,34 @@
 <template>
-  <Navbar />
-  <div class="container">
-    <router-view/>
+  <Cargando v-if="loading" class="mt-5"/>
+  <div v-else>
+    <Navbar />
+    <div class="container">
+      <router-view/>
+    </div>
   </div>
+  
 </template>
 
 <script>
 import Navbar from '../src/components/Navbar.vue'
+import Cargando from '@/components/Cargando.vue'
+import { firebase } from '@/firebase'
+import {onMounted, ref} from 'vue' 
 export default {
   components:{
-    Navbar
+    Navbar,
+    Cargando
   },
   setup() {
-    
+    const loading = ref(false)
+
+    onMounted(async() => {
+      loading.value = true;
+      await firebase.getCurrenUser()
+      loading.value = false;
+    })
+
+    return{ loading }
   },
 }
 </script>
---
+++ src/components/Cargando.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="d-flex justify-content-center">
+    <div class="spinner-border text-info" role="status">
+        <span class="visually-hidden">Loading...</span>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
